e14210133417e3320c7de765afb9a42a63512fec
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2018-07-07 13:34:47 +1000

    Add with_data creator fn for cpu::Core

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,19 +1,31 @@
+use self::memory::{Memory, MemorySpace};
 use self::register::Registers;
 
 mod memory;
 mod register;
 
-/// Frequency of 6502 processor is ~1.79 MHz.
+/// Frequency of the 6502 processor is ~1.79 MHz.
 const FREQUENCY: usize = 1_789_773;
 
 pub struct Core {
     pub reg: Registers,
+    pub memory: Memory,
 }
 
 impl Default for Core {
     fn default() -> Self {
         Core {
             reg: Registers::default(),
+            memory: Memory::default(),
+        }
+    }
+}
+
+impl Core {
+    pub fn with_data(data: MemorySpace) -> Self {
+        Core {
+            reg: Registers::default(),
+            memory: Memory::with_data(data),
         }
     }
 }
