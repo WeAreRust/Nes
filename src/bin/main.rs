bf547a04862382e161460cd64e797a9a32902fbc
Toby Sullivan <tobyjsullivan@gmail.com>
2018-07-10 22:34:03 +1000

    Read ROM file

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Must supply ROM filename.");
+    }
+
+    let filename = &args[1];
+    println!("Loading ROM: {}", filename);
+    let mut f = File::open(filename).expect("File not found");
+    let mut data: Vec<u8> = vec![];
+    f.read_to_end(&mut data).unwrap();
+
+    // TODO(toby): parse the file content
+    // let cartridge = file::parse_rom_file(data[..]);
+
     println!("NES Emulator");
 }
