505dd43f70265ea097cd891d017e7c4f2c90c6b4
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2018-07-11 22:35:10 +1000

    Add nes_asm macro

--- src/cpu/instruction/mod.rs
+++ src/cpu/instruction/mod.rs
@@ -1,1 +1,10 @@
+#[macro_export]
+macro_rules! nes_asm {
+    ($e:expr) => {{
+        let mut buf = vec![];
+        ::asm6502::assemble($e.as_bytes(), &mut buf).unwrap();
+        buf
+    }};
+}
+
 mod jmp;
