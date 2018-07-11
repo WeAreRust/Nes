b097246caaa17562a6e90751a9963ac3de657eb3
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2018-07-11 19:52:28 +1000

    Update executable to processor trait

--- src/clock.rs
+++ src/clock.rs
@@ -1,5 +1,24 @@
 use memory::Memory;
 
-pub trait Executable {
-    fn execute(&mut self, memory: &mut Memory);
+/// NTSC master clock frequency (per second)
+pub const MASTER_FREQUENCY: usize = 21_477_272;
+
+/// NTSC CPU frequency factor
+pub const CPU_FREQUENCY: usize = 12;
+
+/// NTSC PPU frequency factor
+pub const PPU_FREQUENCY: usize = 4;
+
+pub trait Processor {
+    fn cycle(&mut self, memory: &mut Memory);
+}
+
+pub struct Clock {
+    tick: usize,
+}
+
+impl Clock {
+    pub fn cycle() {
+        unimplemented!();
+    }
 }
