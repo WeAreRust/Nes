46121c90d9ca0c4e7fd4fce80da328e944570f4e
Toby Sullivan <1534986+tobyjsullivan@users.noreply.github.com>
2018-07-26 09:03:14 +1000

    Extract console to a struct (#32)

--- nes/src/bin/main.rs
+++ nes/src/bin/main.rs
@@ -5,8 +5,7 @@ use std::fs::File;
 use std::io::Read;
 use std::time::Instant;
 
-use nes::clock::Clock;
-use nes::clock::Processor;
+use nes::console::Console;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -34,37 +33,6 @@ fn main() {
 
     println!("Cartridge loaded.");
 
-    let mut clock = Clock::new();
-    let mut cpu = nes::cpu::Core::default();
-    let mut ram = nes::memory::block::BlockMemory::with_size(0x0800);
-    let mut bus = nes::bus::Bus::new(&mut cartridge, &mut ram);
-    let mut cpu_interval: u8 = 0;
-    let mut ppu_interval: u8 = 0;
-    let mut total_cycles: u64 = 0;
-    let mut report_cycle: u32 = 0;
-    let start = Instant::now();
-
-    cpu.reset(&bus);
-    loop {
-        clock.cycle();
-        cpu_interval += 1;
-        ppu_interval += 1;
-
-        if cpu_interval == nes::clock::CPU_PERIOD {
-            cpu_interval = 0;
-            cpu.cycle(&mut bus);
-        }
-
-        if ppu_interval == nes::clock::PPU_FREQUENCY {
-            ppu_interval = 0;
-            // TODO: ppu.cycle()
-        }
-
-        total_cycles += 1;
-        report_cycle += 1;
-        if report_cycle == nes::clock::MASTER_FREQUENCY {
-            report_cycle = 0;
-            println!("Total cycles: {}; time: {:?} secs", total_cycles, Instant::now().duration_since(start));
-        }
-    }
+    let mut console = Console{ cartridge: &mut cartridge };
+    console.run();
 }
---
+++ nes/src/console.rs
@@ -0,0 +1,39 @@
+use cartridge::Cartridge;
+use clock::{Clock, Processor};
+use clock;
+
+use cpu::Core;
+use memory::block::BlockMemory;
+use bus::Bus;
+
+pub struct Console<'a> {
+  pub cartridge: &'a mut Cartridge,
+}
+
+impl <'a> Console<'a> {
+  pub fn run(&mut self) {
+    let mut clock = Clock::new();
+    let mut cpu = Core::default();
+    let mut ram = BlockMemory::with_size(0x0800);
+    let mut bus = Bus::new(&mut self.cartridge, &mut ram);
+    let mut cpu_interval: u8 = 0;
+    let mut ppu_interval: u8 = 0;
+
+    cpu.reset(&bus);
+    loop {
+        clock.cycle();
+        cpu_interval += 1;
+        ppu_interval += 1;
+
+        if cpu_interval == clock::CPU_PERIOD {
+            cpu_interval = 0;
+            cpu.cycle(&mut bus);
+        }
+
+        if ppu_interval == clock::PPU_FREQUENCY {
+            ppu_interval = 0;
+            // TODO: ppu.cycle()
+        }
+    }
+  }
+}
