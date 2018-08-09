307635159d88eddb48b4244c7563cdae29e9f2e8
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2018-08-09 22:46:39 +1000

    Implement dex instruction

--- nes/src/cpu/instruction/dex.rs
+++ nes/src/cpu/instruction/dex.rs
@@ -9,7 +9,10 @@ use cpu::{
 /// Flags affected: N, Z
 #[inline(always)]
 fn dex(core: &mut Core) {
-  // TODO: implementation
+  core.reg.x_idx = core.reg.x_idx.wrapping_sub(1);
+
+  core.reg.status.set_zero(core.reg.x_idx);
+  core.reg.status.set_negative(core.reg.x_idx);
 }
 
 /// Decrement index x by one
@@ -25,12 +28,39 @@ pub const IMPLIED: Instruction = Instruction {
 #[cfg(test)]
 mod tests {
   use super::*;
-  use cpu::Registers;
+  use cpu::{register::StatusFlags, Registers};
 
   #[test]
   fn dex_impl() {
     let mut core = Core::new(Registers::empty());
-    // TODO: test
+    core.reg.x_idx = 0xff;
+    dex(&mut core);
+    assert_eq!(core.reg.x_idx, 0xfe);
+  }
+
+  #[test]
+  fn dex_impl_overflow() {
+    let mut core = Core::new(Registers::empty());
+    dex(&mut core);
+    assert_eq!(core.reg.x_idx, 0xff);
+  }
+
+  #[test]
+  fn dex_impl_zero_flag() {
+    let mut core = Core::new(Registers::empty());
+    core.reg.x_idx = 1;
+    dex(&mut core);
+    assert_eq!(core.reg.x_idx, 0);
+    assert!(core.reg.status.contains(StatusFlags::Z_FLAG));
+  }
+
+  #[test]
+  fn dex_impl_negative_flag() {
+    let mut core = Core::new(Registers::empty());
+    core.reg.x_idx = 129;
+    dex(&mut core);
+    assert_eq!(core.reg.x_idx, 128);
+    assert!(core.reg.status.contains(StatusFlags::N_FLAG));
   }
 
   #[test]
