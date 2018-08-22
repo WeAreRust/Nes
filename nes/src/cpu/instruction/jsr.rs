034266753131c7cf1011b493e62e7f09165b29e4
Benjamin Thompson <ben.t@canva.com>
2018-08-22 23:28:48 +1000

    Impl JSR (#60)

--- nes/src/cpu/instruction/jsr.rs
+++ nes/src/cpu/instruction/jsr.rs
@@ -9,9 +9,21 @@ use memory::WriteAddr;
 ///
 /// Flags affected: None
 #[inline(always)]
-fn jsr(core: &mut Core, memory: &mut WriteAddr, address: u16) {
-  // TODO: implementation
-  unimplemented!();
+fn jsr(core: &mut Core, memory: &mut WriteAddr, _address: u16) {
+  let pc_plus_1: u16 = core.reg.pc + 1;
+  let pc_plus_2: u16 = core.reg.pc + 2;
+
+  // push PC + 2
+  let pc_plus_2_hi: u8 = (pc_plus_2 >> 8) as u8;
+  let pc_plus_2_lo: u8 = pc_plus_2 as u8;
+  core.push_stack(memory, pc_plus_2_hi);
+  core.push_stack(memory, pc_plus_2_lo);
+
+  // (PC+1) -> PCL
+  // (PC+2) -> PCH
+  let new_pc_lo: u16 = memory.read_addr(pc_plus_1) as u16;
+  let new_pc_hi: u16 = memory.read_addr(pc_plus_2) as u16;
+  core.reg.pc = new_pc_lo | (new_pc_hi << 8);
 }
 
 /// Jump to new location saving return address
@@ -28,11 +40,20 @@ pub const ABSOLUTE: Instruction = Instruction {
 mod tests {
   use super::*;
   use cpu::Registers;
+  use memory::block::BlockMemory;
 
   #[test]
   fn jsr_impl() {
     let mut core = Core::new(Registers::empty());
-    // TODO: test
+    let mut memory = BlockMemory::with_size(0x0300);
+    core.reg.stack = 0xff; // init stack
+    core.reg.pc = 0x0200;
+    memory.write_addr(0x0201, 0x01); // PC + 1
+    memory.write_addr(0x0202, 0xff); // PC + 2
+    jsr(&mut core, &mut memory, 0x0200);
+    assert_eq!(core.reg.pc, 0xff01);
+    assert_eq!(core.pop_stack(&mut memory), 0x02); // PC + 2 (hi)
+    assert_eq!(core.pop_stack(&mut memory), 0x02); // PC + 2 (lo)
   }
 
   #[test]
